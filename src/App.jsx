a292356a1ae03838b0aeaca1872759d09dbba1b8
stivkingson237-oss <stivkingson237@gmail.com>
2026-08-19 23:10:03 +0100

    Route Create Account through explicit Supabase email/password signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,11 @@
-export { default } from "./MarketplaceShell.jsx";
+import React from 'react';
+import MarketplaceShell from './MarketplaceShell.jsx';
+import AuthGate from './AuthGate.jsx';
+
+export default function App() {
+  return (
+    <AuthGate>
+      <MarketplaceShell />
+    </AuthGate>
+  );
+}
