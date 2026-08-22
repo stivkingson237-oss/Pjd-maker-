ffa731b0392c0b018c2c62299f14ef6726262f69
stivkingson237-oss <stivkingson237@gmail.com>
2026-08-23 00:46:03 +0100

    fix: load PJD Maker interface overrides

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./responsive.css";
 import "./marketplace-extra.css";
+import "./interface-overrides.css";
 import App from "./App.jsx";
 
 const root = createRoot(document.getElementById("root"));
