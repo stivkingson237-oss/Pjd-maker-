e06da656e9a4c5f52ca61fe02b9a8c8c6e4256fd
stivkingson237-oss <stivkingson237@gmail.com>
2026-08-25 16:11:57 +0100

    fix: load marketplace 2.0 styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./responsive.css";
+import "./marketplace.css";
 import "./marketplace-extra.css";
 import "./interface-overrides.css";
 import App from "./App.jsx";
