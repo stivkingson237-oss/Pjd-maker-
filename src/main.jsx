1e55df97165c6fdba3b062f76c41fd68ea857e57
stivkingson237-oss <stivkingson237@gmail.com>
2026-08-28 18:09:22 +0100

    fix: restore global marketplace styles for Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import './responsive.css';
+import './marketplace.css';
+import './marketplace-extra.css';
+import './interface-overrides.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
